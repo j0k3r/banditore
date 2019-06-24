53ae7c218541bfb3f0d7875debed2867e3ffaa7e
Jeremy Benoist <jeremy.benoist@gmail.com>
2019-06-24 12:05:58 +0200

    Update own extension

--- src/AppBundle/Twig/RepoVersionExtension.php
+++ src/AppBundle/Twig/RepoVersionExtension.php
@@ -2,15 +2,18 @@
 
 namespace AppBundle\Twig;
 
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
 /**
  * Took a repo with version information to display a link to that version on Github.
  */
-class RepoVersionExtension extends \Twig_Extension
+class RepoVersionExtension extends AbstractExtension
 {
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('link_to_version', [$this, 'linkToVersion']),
+            new TwigFilter('link_to_version', [$this, 'linkToVersion']),
         ];
     }
 
