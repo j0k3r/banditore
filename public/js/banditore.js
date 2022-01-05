b72a731640a04cc8ef852b9f4fa3caacd0d9d530
Jeremy Benoist <jeremy.benoist@gmail.com>
2022-01-05 10:58:40 +0100

    Fix some issues from Codefactor
    - Unnecessary semicolon
    - Variable assignment found within a condition. Did you mean to do a comparison ?
    - Unexpected unit (length-zero-no-unit)
    - Unexpected duplicate "display" (declaration-block-no-duplicate-properties)

--- public/js/banditore.js
+++ public/js/banditore.js
@@ -21,40 +21,40 @@
 
     // handle rwd menu
     var menu = document.getElementById('menu'),
-    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize'
 
     function toggleHorizontal() {
         [].forEach.call(
             document.getElementById('menu').querySelectorAll('.menu-can-transform'),
             function(el){
-                el.classList.toggle('pure-menu-horizontal');
+                el.classList.toggle('pure-menu-horizontal')
             }
-        );
-    };
+        )
+    }
 
     function toggleMenu() {
         // set timeout so that the panel has a chance to roll up
         // before the menu switches states
         if (menu.classList.contains('open')) {
-            setTimeout(toggleHorizontal, 500);
+            setTimeout(toggleHorizontal, 500)
         } else {
-            toggleHorizontal();
+            toggleHorizontal()
         }
 
-        menu.classList.toggle('open');
-        document.getElementById('toggle').classList.toggle('x');
-    };
+        menu.classList.toggle('open')
+        document.getElementById('toggle').classList.toggle('x')
+    }
 
     function closeMenu() {
         if (menu.classList.contains('open')) {
-            toggleMenu();
+            toggleMenu()
         }
     }
 
     document.getElementById('toggle').addEventListener('click', function (e) {
-        toggleMenu();
-        e.preventDefault();
-    });
+        toggleMenu()
+        e.preventDefault()
+    })
 
-    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-})(this, this.document);
+    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu)
+})(this, this.document)
