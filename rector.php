393bcce7cdb3619534b95f493ed0d6ea4fb722e9
Jeremy Benoist <jeremy.benoist@gmail.com>
2026-04-20 14:14:16 +0200

    Fix CS

--- rector.php
+++ rector.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
